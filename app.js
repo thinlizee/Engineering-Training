c3bd8636c14ba66b47b6c1ffab5c102c28102b01
Elizabeth Ward <eward@totalwine.com>
2023-08-25 11:37:38 -0400

    TT-136: Express - Routing setup

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+const router = express.Router();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//connect the router to express app
+app.use('/', router);
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
