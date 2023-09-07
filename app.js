f1f2b6bb7dae11db4330b46fb01d7b56cb7a3d0b
Elizabeth Ward <eward@totalwine.com>
2023-09-07 10:44:01 -0400

    TT-138: Express - Create a GET endpoint

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const router = express.Router();
 
 //connect the router to express app
 app.use('/', router);
+app.use(express.static('public'));
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
-
-app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+});
+
+router.get('/getJiraTickets', (req, res) => {
+  res.json({jirasObj:[]});
 });
