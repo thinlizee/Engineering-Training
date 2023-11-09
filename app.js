f7f3e86e290ee2ffccf289093077b7dc13b9756d
Elizabeth Ward <eward@totalwine.com>
2023-11-09 13:42:27 -0500

    TT-140: changing router.get and setting response

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const app = express()
 const port = 3000
 const path = require('path');
 const router = express.Router();
-const dataHandler = require('./dataHandler');
+const dataHandler = require("./dataHandler.js");
 
 //connect the router to express app
 app.use('/', router);
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
 
-router.get('/getJiraTickets', (req, res) => {
-  res.json({dataHandler:jirasObj});
+router.get("/dataHandler", (req, res) => {
+  res.json({jirasObj:dataHandler.jirasObj})
 });
