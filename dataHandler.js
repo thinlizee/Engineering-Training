7530257725ef076ef8dd9d609f8fddf60a9aaf86
Elizabeth Ward <eward@totalwine.com>
2023-12-09 14:53:44 -0500

    TT-144: Regex and indexOf

--- dataHandler.js
+++ dataHandler.js
@@ -2,30 +2,10 @@ const { Octokit } = require("@octokit/rest");
 require('dotenv').config()
 var JiraApi = require('jira-client');
 
-let jiraLinks = ['https://totalwine.atlassian.net/browse/TT-2',
-'https://totalwine.atlassian.net/browse/TT-16',
-'https://totalwine.atlassian.net/browse/TT-17',
-'https://totalwine.atlassian.net/browse/TT-18',
-'https://totalwine.atlassian.net/browse/TT-19'
-];
-console.log('Jira Links', jiraLinks);
-
-let jiraTitles = ['Create a public repository under your GitHub account',
-'Create a new script file, and import it into index.html and add a console log',
-'JavaScript: Variables',
-'JavaScript: Event Listeners - Add Toggle Button Inside of Modal',
-'JavaScript: Functions - Write a function to toggle hidden class on modal'
-];
-console.log('Jira Titles', jiraTitles);
-
 const errorJiraTemplate = { icon: 'bi bi-x-circle' };
 const jiraTemplate = { icon: 'bi bi-check-circle-fill' };
 
-function getIcon() {
-   let rNum = getRandomIntInclusive(0,2);
-   return rNum == 1 ? jiraTemplate : errorJiraTemplate;
- }
-
+let jiraNums = [];
 class DataHandler {
     constructor(links, title ) {
       this.links = links;
@@ -44,28 +24,59 @@ class DataHandler {
       });
      }
      return this.jirasObj;
- } 
-    fetchGitHubData() {
-      return new Promise((resolve)=>{
-        const octokit = new Octokit({
-          auth: process.env.GITHUB_TOKEN
-        });
-      octokit.rest.repos.listCommits({
+    } 
+  async fetchGitHubData() {
+   const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+    });
+
+   try {
+    const response = await octokit.rest.repos.listCommits({
       owner: "thinlizee",
       repo: "engineering-training",
-      })
-    .then((response) => {
-      for (let i = 0; i < 20; i++) {
-        console.log("Commit message: ", response.data[i].commit.message);
-      }}
-    )}
-    )}
-}; 
+    });
+
+    for (let i = 0; i < response.data.length; i++) {
+      const regex = /[A-Z][A-Z]+-\d+/g;
+      const ticketNum = response.data[i].commit.message.match(regex);
+
+      if (ticketNum !== null && jiraNums.indexOf(ticketNum[0]) == -1) {
+        jiraNums.push(ticketNum[0]);
+      }
+    }
+  } 
+   catch (error) {
+    console.log("Error getting data:", error.message);
+  } 
+  console.log(jiraNums);
+}
+}
 
 function getRandomIntInclusive(min, max) {
    min = Math.ceil(min);
    max = Math.floor(max);
    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+};
+
+let jiraLinks = ['https://totalwine.atlassian.net/browse/TT-2',
+'https://totalwine.atlassian.net/browse/TT-16',
+'https://totalwine.atlassian.net/browse/TT-17',
+'https://totalwine.atlassian.net/browse/TT-18',
+'https://totalwine.atlassian.net/browse/TT-19'
+];
+console.log('Jira Links', jiraLinks);
+
+let jiraTitles = ['Create a public repository under your GitHub account',
+'Create a new script file, and import it into index.html and add a console log',
+'JavaScript: Variables',
+'JavaScript: Event Listeners - Add Toggle Button Inside of Modal',
+'JavaScript: Functions - Write a function to toggle hidden class on modal'
+];
+console.log('Jira Titles', jiraTitles);
+
+function getIcon() {
+  let rNum = getRandomIntInclusive(0,2);
+  return rNum == 1 ? jiraTemplate : errorJiraTemplate;
 };
 
 const dataHandler = new DataHandler(jiraLinks, jiraTitles);
